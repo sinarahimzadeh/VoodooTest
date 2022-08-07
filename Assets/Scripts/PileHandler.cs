8c3a819937b8ea6aa8a1e8846cc0b3d0866b986c
sinarahimzadeh <sinarahimzade@gmail.com>
2022-08-07 17:30:04 +0430

    some basics

--- Assets/Scripts/PileHandler.cs
+++ Assets/Scripts/PileHandler.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class PileHandler : MonoBehaviour
 {
 public Rigidbody rigidbody;
+
+public static PileHandler instance; 
 [SerializeField] private float force;
+    public enum States
+    {
+        onRail,
+        inAir,
+    };
+
+   public  States states= new States();
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance=this;
+     states=States.onRail;   
+    }
+
+     private void OnCollisionEnter(Collision other) {
+        if (other.transform.tag=="rail"){states=States.onRail;}
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        rigidbody.AddForce(Vector3.up*force*Time.deltaTime);
-        //hi
-        //hiagain
+        if(Input.GetMouseButtonDown(0)){
+            if(states!=States.inAir)
+            jump();
+
+        }
         
+
     }
+
+    public void jump(){
+        states=States.inAir;
+         rigidbody.AddForce(Vector3.up*force*Time.deltaTime);
+         }
 }
