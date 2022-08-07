af84c9b138af188e49758b9320028c7c44dd1cf6
sinarahimzadeh <sinarahimzade@gmail.com>
2022-08-07 18:23:25 +0430

    jump first version

--- Assets/Scripts/PileHandler.cs
+++ Assets/Scripts/PileHandler.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class PileHandler : MonoBehaviour
 {
 public Rigidbody rigidbody;
-
+private bool fall;
+public float maxY,currentY,upRate ; 
 public static PileHandler instance; 
 [SerializeField] private float force;
     public enum States
     {
         onRail,
         inAir,
+        falling
     };
 
    public  States states= new States();
     // Start is called before the first frame update
     void Start()
     {
-        instance=this;
-     states=States.onRail;   
+    
+    instance=this;
+     currentY=this.gameObject.transform.position.y;   
     }
 
-     private void OnCollisionEnter(Collision other) {
+     public void OnCollisionEnter(Collision other) {
         if (other.transform.tag=="rail"){states=States.onRail;}
     }
 
@@ -30,12 +33,22 @@ public static PileHandler instance;
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            if(states!=States.inAir)
-            jump();
+            if(states==States.onRail){
+            currentY=transform.position.y;
+            jump();}
 
         }
         
+        if(states==States.inAir){
+           if( currentY+maxY>=transform.position.y){
+            transform.position=new Vector3(transform.position.x,transform.position.y+upRate,transform.position.z);
+           }
+           else{
+               states=States.falling;
+           }
+        }
 
+    
     }
 
     public void jump(){
