a49d003b5f05a3367e82f7885c48e1dc81aadc96
KeyvanTheLegend <kayvan.yaghoubian@gmail.com>
2022-08-09 22:26:38 +0430

    merged

---
+++ Assets/Scripts/PileHandler.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PileHandler : MonoBehaviour
+{
+    [SerializeField] private int counter;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "gate")
+        {
+            counter++;
+            // Time.timeScale = 0;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "gate" && counter < 2)
+        {
+            counter = 0;
+            // Time.timeScale = 0;
+        }
+    }
+
+    void Update()
+    {
+        if (counter >= 2)
+        {
+            GameManager.shared.Lose();
+        }
+    }
+}
